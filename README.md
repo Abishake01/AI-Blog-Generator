1e0b2f78443a2cd6e05385da11965954af686d0c
Abishake01 <abishake381@gmail.com>
2024-11-12 10:14:10 +0530

    Remove OpenAI API key

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 <h1>AI-Blog Generator</h1>
+username:admin
+email:admin12@gmail.com
+pwd:Add123
--- backend/ai_blog_app/blog_generator/urls.py
+++ backend/ai_blog_app/blog_generator/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from .import views
 urlpatterns =[
-    path('',views.index,name='index')
+    path('',views.index,name='home'),
+    path('login',views.log_in,name='login'),
+    path('signup',views.sign_up,name='signup'),
+    path('all_blogs',views.all_blogs,name='all_blogs'),
+    path('blog_details',views.blog_de,name='blog_details'),
+    path('logout',views.log_out,name='logout'),
+    path("generate-blog/", views.generate_blog, name="generate_blog"),
 ]
